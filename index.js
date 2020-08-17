6876d4677f3d361ec8b3570d92f8ee72baf3030b
dazelpro <dazelpro@gmail.com>
2020-08-17 21:20:06 +0700

    building env

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express       = require('express');
 const app           = express();
 
-app.listen(8080, ()=>{
-    console.log('Server Berjalan di Port : 8080');
+require('dotenv').config();
+const { 
+    NODE_ENV, 
+    PORT_SERVER_DEV, 
+    PORT_SERVER_PRO
+} = process.env;
+  
+let PORT;
+
+// if (NODE_ENV === 'production') {
+//     PORT = PORT_SERVER_PRO
+// } else {
+//     PORT = PORT_SERVER_DEV
+// }
+
+(NODE_ENV === 'production')?PORT = PORT_SERVER_PRO :PORT = PORT_SERVER_DEV
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening in port : ${PORT}`);
 });
