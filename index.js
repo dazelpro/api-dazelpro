7cfd529f0199e2747d2abb2c421910bc71ca6680
dazelpro <dazelpro@gmail.com>
2020-08-17 21:28:30 +0700

    fix logic

--- index.js
+++ index.js
@@ -2,20 +2,9 @@ const express       = require('express');
 const app           = express();
 
 require('dotenv').config();
-const { 
-    NODE_ENV, 
-    PORT_SERVER_DEV, 
-    PORT_SERVER_PRO
-} = process.env;
-  
+const { NODE_ENV, PORT_SERVER_DEV, PORT_SERVER_PRO } = process.env;
+
 let PORT;
-
-// if (NODE_ENV === 'production') {
-//     PORT = PORT_SERVER_PRO
-// } else {
-//     PORT = PORT_SERVER_DEV
-// }
-
 (NODE_ENV === 'production')?PORT = PORT_SERVER_PRO :PORT = PORT_SERVER_DEV
 
 app.listen(PORT, ()=>{
