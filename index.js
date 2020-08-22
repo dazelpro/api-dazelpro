e51109849685ee80ed649f523160f1fe5423c8d7
dazelpro <dazelpro@gmail.com>
2020-08-22 08:09:07 +0700

    add depedensi

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express       = require('express');
+const morgan        = require('morgan');
+const helmet        = require('helmet');
+const bodyParser    = require('body-parser');
 const app           = express();
 
 require('dotenv').config();
@@ -6,6 +9,12 @@ const { NODE_ENV, PORT_SERVER_DEV, PORT_SERVER_PRO } = process.env;
 
 let PORT;
 (NODE_ENV === 'production')?PORT = PORT_SERVER_PRO :PORT = PORT_SERVER_DEV
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(helmet())
+app.use(morgan('dev'));
 
 app.listen(PORT, ()=>{
     console.log(`Server listening in port : ${PORT}`);
