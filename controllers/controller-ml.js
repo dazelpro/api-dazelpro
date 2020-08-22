606181b5f33dfbbc8714d94f53064073529c76a0
dazelpro <dazelpro@gmail.com>
2020-08-22 14:52:23 +0700

    get hero by id

--- controllers/controller-ml.js
+++ controllers/controller-ml.js
@@ -51,5 +51,49 @@ module.exports ={
             });
             connection.release();
         })
+    },
+    getHeroById(req,res){
+        let hero = [];
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `SELECT * FROM table_hero_ml WHERE hero_id = ${req.params["id"]}`
+            , function (error, results) {
+                if (error) {
+                    console.error(error);
+                    return res.status(400).send({
+                        success: false,
+                        status: 400,
+                        message: error
+                    });	
+                }
+                
+                results.forEach(async function(heroloop) {
+                    let data = {
+                        hero_id : heroloop['hero_id'],
+                        hero_name : heroloop['hero_name'],
+                        hero_avatar : heroloop['hero_avatar'],
+                        hero_role : heroloop['hero_role'],
+                        hero_specially : heroloop['hero_specially'],
+                        hero_overview : {
+                            hero_durability : heroloop['hero_durability'],
+                            hero_offence : heroloop['hero_offence'],
+                            hero_ability : heroloop['hero_ability'],
+                            hero_difficulty : heroloop['hero_difficulty'],
+                        }
+                    }
+                    hero.push(data)
+                })
+                
+                res.status(200).send({
+                    success: true,
+                    status: 200,
+                    rowCount: results.length,
+                    message: "Successful",
+                    hero: hero
+                });
+            });
+            connection.release();
+        })
 	},
 }
--- routes/router-ml.js
+++ routes/router-ml.js
@@ -2,5 +2,6 @@ const router                = require('express').Router();
 const mobileLegends         = require('../controllers').mobileLegends;
 
 router.get('/hero', mobileLegends.getHero);
+router.get('/hero/:id', mobileLegends.getHeroById);
 
 module.exports = router;
