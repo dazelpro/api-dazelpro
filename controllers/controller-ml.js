a83437aab7d09984c3238643806dcf6404ac9ee6
dazelpro <dazelpro@gmail.com>
2020-08-22 13:16:17 +0700

    custom json

--- controllers/controller-ml.js
+++ controllers/controller-ml.js
@@ -9,10 +9,11 @@ pool.on('error',(err)=> {
 
 module.exports ={
     getHero(req,res){
+        let hero = [];
         pool.getConnection(function(err, connection) {
             if (err) throw err;
             connection.query(
-                `SELECT* FROM table_hero_ml`
+                `SELECT * FROM table_hero_ml`
             , function (error, results) {
                 if (error) {
                     console.error(error);
@@ -22,11 +23,29 @@ module.exports ={
                         message: error
                     });	
                 }
+                
+                results.forEach(async function(heroloop) {
+                    let data = {
+                        hero_id : heroloop['hero_id'],
+                        hero_name : heroloop['hero_name'],
+                        hero_avatar : heroloop['hero_avatar'],
+                        hero_role : heroloop['hero_role'],
+                        hero_specially : heroloop['hero_specially'],
+                        hero_overview : {
+                            hero_durability : heroloop['hero_durability'],
+                            hero_offence : heroloop['hero_offence'],
+                            hero_ability : heroloop['hero_ability'],
+                            hero_difficulty : heroloop['hero_difficulty'],
+                        }
+                    }
+                    hero.push(data)
+                })
+                
                 res.status(200).send({
                     success: true,
                     status: 200,
                     message: "Successful",
-                    hero: results
+                    hero: hero
                 });
             });
             connection.release();
