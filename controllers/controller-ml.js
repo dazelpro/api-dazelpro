56b4e9d2188da388f1f0f1026b03021d802bf0ea
dazelpro <dazelpro@gmail.com>
2020-08-22 11:18:05 +0700

    get hero controller

---
+++ controllers/controller-ml.js
@@ -0,0 +1,36 @@
+const router    = require('express').Router();
+const config    = require('../config/database');
+
+let mysql      = require('mysql');
+let pool       = mysql.createPool(config);
+
+pool.on('error',(err)=> {
+    console.error(err);
+});
+
+module.exports ={
+    getHero(req,res){
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `SELECT* FROM table_hero_ml`
+            , function (error, results) {
+                if (error) {
+                    console.error(error);
+                    return res.status(400).send({
+                        success: false,
+                        status: 400,
+                        message: error
+                    });	
+                }
+                res.status(200).send({
+                    success: true,
+                    status: 200,
+                    message: "Successful",
+                    hero: results
+                });
+            });
+            connection.release();
+        })
+	},
+}
--- routes/router-ml.js
+++ routes/router-ml.js
@@ -1,3 +1,6 @@
 const router                = require('express').Router();
-const accountController     = require('../controllers').user;
+const mobileLegends         = require('../controllers').mobileLegends;
+
+router.get('/hero', mobileLegends.getHero);
+
 module.exports = router;
