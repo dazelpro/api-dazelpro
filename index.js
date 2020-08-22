dbb147a8f4a00c63ba01c510432e4e13e6ba5733
dazelpro <dazelpro@gmail.com>
2020-08-22 09:50:05 +0700

    manage versionering

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ const { NODE_ENV, PORT_SERVER_DEV, PORT_SERVER_PRO } = process.env;
 let PORT;
 (NODE_ENV === 'production')?PORT = PORT_SERVER_PRO :PORT = PORT_SERVER_DEV
 
+const mlRoutes       = require('./routes/router-ml');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(helmet())
 app.use(morgan('dev'));
+
+app.get('/',(req,res)=>{
+    res.send({'API Version': '1.0.0 Beta'});
+});
+
+app.use('/mobile-legends',mlRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Server listening in port : ${PORT}`);
