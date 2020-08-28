3f3828385d60b02409a282bfbee3c084d132a178
dazelpro <dazelpro@gmail.com>
2020-08-28 21:11:05 +0700

    add env

--- controllers/controller-ml.js
+++ controllers/controller-ml.js
@@ -7,44 +7,98 @@ pool.on('error',(err)=> {
     console.error(err);
 });
 
+require('dotenv').config();
+const { 
+    NODE_ENV, 
+    URL_IMG_DEV, 
+    URL_IMG_PRO,
+} = process.env;
+  
+let urlImage;
+  
+if (NODE_ENV === 'production') {
+    urlImage = URL_IMG_PRO
+} else {
+    urlImage = URL_IMG_DEV
+}
+
 module.exports ={
     getHero(req,res){
         let hero = [];
-        pool.getConnection(function(err, connection) {
-            if (err) throw err;
-            connection.query(
-                `SELECT * FROM table_hero_ml`
-            , function (error, results) {
-                if (error) {
-                    console.error(error);
-                    return res.status(400).send({
-                        success: false,
-                        status: 400,
-                        message: error
-                    });	
-                }
-                
-                results.forEach(async function(heroloop) {
-                    let data = {
-                        hero_id : heroloop['hero_id'],
-                        hero_name : heroloop['hero_name'],
-                        hero_avatar : heroloop['hero_avatar'],
-                        hero_role : heroloop['hero_role'],
-                        hero_specially : heroloop['hero_specially']
+        let keyword = req.query['heroName'];
+        if(req.query['heroName'] === undefined){
+            pool.getConnection(function(err, connection) {
+                if (err) throw err;
+                connection.query(
+                    `SELECT * FROM table_hero_ml`
+                , function (error, results) {
+                    if (error) {
+                        console.error(error);
+                        return res.status(400).send({
+                            success: false,
+                            status: 400,
+                            message: error
+                        });	
                     }
-                    hero.push(data)
-                })
-                
-                res.status(200).send({
-                    success: true,
-                    status: 200,
-                    rowCount: results.length,
-                    message: "Successful",
-                    hero: hero
+                    
+                    results.forEach(async function(heroloop) {
+                        let data = {
+                            hero_id : heroloop['hero_id'],
+                            hero_name : heroloop['hero_name'],
+                            hero_avatar : `${urlImage}files/images/${heroloop['hero_avatar']}`,
+                            hero_role : heroloop['hero_role'],
+                            hero_specially : heroloop['hero_specially']
+                        }
+                        hero.push(data)
+                    })
+                    
+                    res.status(200).send({
+                        success: true,
+                        status: 200,
+                        rowCount: results.length,
+                        message: "Successful",
+                        hero: hero
+                    });
                 });
-            });
-            connection.release();
-        })
+                connection.release();
+            })
+        }else{
+            pool.getConnection(function(err, connection) {
+                if (err) throw err;
+                connection.query(
+                    `SELECT * FROM table_hero_ml WHERE hero_name LIKE '%${keyword}%'`
+                , function (error, results) {
+                    if (error) {
+                        console.error(error);
+                        return res.status(400).send({
+                            success: false,
+                            status: 400,
+                            message: error
+                        });	
+                    }
+                    
+                    results.forEach(async function(heroloop) {
+                        let data = {
+                            hero_id : heroloop['hero_id'],
+                            hero_name : heroloop['hero_name'],
+                            hero_avatar : `${urlImage}files/images/${heroloop['hero_avatar']}`,
+                            hero_role : heroloop['hero_role'],
+                            hero_specially : heroloop['hero_specially']
+                        }
+                        hero.push(data)
+                    })
+                    
+                    res.status(200).send({
+                        success: true,
+                        status: 200,
+                        rowCount: results.length,
+                        message: "Successful",
+                        hero: hero
+                    });
+                });
+                connection.release();
+            })
+        }
     },
     getHeroById(req,res){
         let hero = [];
@@ -66,7 +120,7 @@ module.exports ={
                     let data = {
                         hero_id : heroloop['hero_id'],
                         hero_name : heroloop['hero_name'],
-                        hero_avatar : heroloop['hero_avatar'],
+                        hero_avatar : `${urlImage}files/images/${heroloop['hero_avatar']}`,
                         hero_role : heroloop['hero_role'],
                         hero_specially : heroloop['hero_specially'],
                         hero_overview : {
@@ -136,7 +190,7 @@ module.exports ={
                         let data = {
                             hero_id : heroloop['hero_id'],
                             hero_name : heroloop['hero_name'],
-                            hero_avatar : heroloop['hero_avatar'],
+                            hero_avatar : `${urlImage}files/images/${heroloop['hero_avatar']}`,
                             hero_role : heroloop['hero_role'],
                             hero_specially : heroloop['hero_specially'],
                             hero_overview : {
@@ -207,7 +261,7 @@ module.exports ={
                         let data = {
                             hero_id : heroloop['hero_id'],
                             hero_name : heroloop['hero_name'],
-                            hero_avatar : heroloop['hero_avatar'],
+                            hero_avatar : `${urlImage}files/images/${heroloop['hero_avatar']}`,
                             hero_role : heroloop['hero_role'],
                             hero_specially : heroloop['hero_specially'],
                             hero_overview : {
