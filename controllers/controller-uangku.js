d2bf786c3e753acf0e1e895fd49017ab2ab2dde0
dazelpro <dazelpro@gmail.com>
2020-11-04 18:01:32 +0700

    + asycn

--- controllers/controller-uangku.js
+++ controllers/controller-uangku.js
@@ -1,5 +1,60 @@
+const async = require("async");
+const config = require("../config/database");
+
+let mysql = require("mysql");
+const { user } = require("../config/database");
+let pool = mysql.createPool(config);
+
+pool.on("error",(err)=> {
+    console.error(err);
+});
+
 module.exports ={
     auth(req,res){
-        console.log(req.body)
+        async.waterfall([
+            (done) => {
+                // Cek User
+                pool.getConnection(function(err, connection) {
+                    if (err) throw err;
+                    connection.query(
+                        `
+                        SELECT * FROM uang_users where userStatus = 0;
+                        `
+                    , function (err, userCek) {
+                        if (err)
+                        return res.status(400).send({
+                            success: false,
+                            message: err
+                        });
+                        done(err, userCek);
+                    });
+                    connection.release();
+                })
+            },
+            (userCek, done) => {
+                console.log(userCek[0])
+                pool.getConnection(function(err, connection) {
+                    if (err) throw err;
+                    connection.query(
+                        `
+                        SELECT * FROM uang_category;
+                        `
+                    , function (err, category) {
+                        if (err)
+                        return res.status(400).send({
+                            success: false,
+                            message: err
+                        });
+                        return res.status(200).send({
+                            success: true,
+                            data: userCek[0]
+                        });
+                    });
+                    connection.release();
+                })
+            }
+        ], (err) => {
+            if (err) return next(err);
+        });
     }
 }
