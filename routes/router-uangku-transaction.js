28c094c407c25e0994c9f0c572e01f3875fc4141
dazelpro <dazelpro@gmail.com>
2020-11-21 21:00:38 +0700

    route

--- routes/router-uangku-transaction.js
+++ routes/router-uangku-transaction.js
@@ -3,13 +3,13 @@ const uangKu = require('../controllers').uangKu;
 
 router.get('/in/:id', uangKu.getDataTransactionIn);
 router.get('/out/:id', uangKu.getDataTransactionOut);
+router.get('/delete-all-in', uangKu.deleteTransactionAllIn);
+router.get('/delete-all-out', uangKu.deleteTransactionAllOut);
 router.post('/insert-in', uangKu.insertTransactionIn);
 router.post('/insert-out', uangKu.insertTransactionOut);
 router.post('/update-in', uangKu.updateTransactionIn);
 router.post('/update-out', uangKu.updateTransactionOut);
-router.post('/delete-in', uangKu.deleteTransactionAllIn);
-router.post('/delete-all-in', uangKu.deleteTransactionIn);
+router.post('/delete-in', uangKu.deleteTransactionIn);
 router.post('/delete-out', uangKu.deleteTransactionOut);
-router.post('/delete-all-out', uangKu.deleteTransactionAllOut);
 
 module.exports = router;
